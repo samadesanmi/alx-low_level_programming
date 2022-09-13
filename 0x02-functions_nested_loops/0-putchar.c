5d4f5424754b9b38837d068330f63d622775b7e8
Samuel Adegbusi <samuel.adegbusi@gmail.com>
2022-09-13 15:47:38 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,14 @@
  *  * main - Prints _putchar as a message.
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
 		int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 			int count, sz;
 
 				sz = sizeof(str) / sizeof(int);
-					for (count = 0; count < sz; count++)
+				for (count = 0; count < sz; count++)
 							{
 										_putchar(str[count]);
 											}
