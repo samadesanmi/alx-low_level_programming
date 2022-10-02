8f2fb08c6874dbdf191fe0a3d7d4873d538d62ec
Samuel Adegbusi <samuel.adegbusi@gmail.com>
2022-10-02 15:48:27 -0700

    Static library

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: string to search
+ * @accept: bytes
+ *
+ * Return: number of bytes in the initial segment of s
+ * which consists only of bytes from accept
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int x, y, m;
+
+	for (x = 0; *(s + x) != '\0'; x++)
+	{
+		m = 0;
+		for (y = 0; *(accept + y) != '\0'; y++)
+		{
+			if (*(s + x) == *(accept + y))
+			{
+				m = 1;
+				break;
+			}
+		}
+		if (!m)
+			return (x);
+	}
+	return (0);
+}
